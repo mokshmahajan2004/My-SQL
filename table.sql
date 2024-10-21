497cbb7283c55b3260f4f91881779ed2d0913476
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-10-22 00:14:52 +0530

    Table Added

---
+++ table.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE college;
+USE college;
+
+create TABLE student(
+rollno INT PRIMARY KEY,
+name VARCHAR(50));
+
+insert into student
+(rollno,name)
+values
+(101,"Karan"),
+(102,"Arjun"),
+(103,"Ram");
+select * from student;
+insert into student values(104,"Shyam")
